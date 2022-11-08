e42605f99177b3520b3705def2154316aee9feab
Matasz21 <matas.sabaliauskas@gmail.com>
2022-11-08 22:48:58 +0200

    modified:   package-lock.json 	modified:   package.json 	new file:   public/LogoStr.png 	deleted:    public/favicon.ico 	new file:   public/images/avieteindelis.jpg 	new file:   public/images/braskeindelis.jpg 	new file:   public/images/melons.jpg 	new file:   public/images/pears.jpg 	new file:   public/images/silauogeindelis.jpg 	new file:   public/images/watermelons.jpg 	modified:   public/index.html 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	deleted:    public/manifest.json 	deleted:    public/robots.txt 	modified:   src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/LogoWeb.png 	new file:   src/contents/Carousel/Owl.jsx 	new file:   src/contents/Data.json 	new file:   src/contents/Footer/footer.css 	new file:   src/contents/Footer/footer.jsx 	new file:   src/contents/header.css 	new file:   src/contents/header.jsx 	new file:   src/contents/images/berriesorder.jpg 	new file:   src/contents/images/blueberries.jpg 	new file:   src/contents/images/raspberries.jpg 	new file:   src/contents/images/strawberries.jpg 	new file:   src/contents/modal/context.jsx 	new file:   src/contents/modal/form.jsx 	new file:   src/contents/modal/modal.css 	new file:   src/contents/modal/modal.jsx 	new file:   src/contents/navBar/Navbar.jsx 	new file:   src/contents/productPreview/Data.json 	new file:   src/contents/productPreview/productPreview.css 	new file:   src/contents/productPreview/productPreview.jsx 	new file:   src/contents/productWhole/productWhole.jsx 	new file:   src/contents/reviews/atsiliepimai.js 	new file:   src/contents/reviews/reviews.css 	new file:   src/contents/reviews/reviews.jsx 	new file:   src/contents/searchBar/SearchBar.css 	new file:   src/contents/searchBar/SearchBar.jsx 	new file:   src/contents/whyChoose/whyChoose.css 	new file:   src/contents/whyChoose/whyChoose.jsx 	deleted:    src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	new file:   src/pages/about/about.css 	new file:   src/pages/about/about.jsx 	new file:   src/pages/contact/contact.css 	new file:   src/pages/contact/contact.jsx 	new file:   src/pages/gallery/gallery.jsx 	new file:   src/pages/home/home.jsx 	new file:   src/pages/products/products.jsx 	deleted:    src/setupTests.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
+import Home from "./pages/home/home";
+import Products from "./pages/products/products.jsx";
+import Gallery from "./pages/gallery/gallery";
+import About from "./pages/about/about";
+import Contact from "./pages/contact/contact";
+import Header from "./contents/header";
+import Footer from "./contents/Footer/footer";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Router>
+    <Header/>
+        <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/products" element={<Products/>} />
+            <Route path="/gallery" element={<Gallery/>} />
+            <Route path="/about" element={<About/>} />
+            <Route path="/contact" element={<Contact/>} />
+        </Routes>
+    <Footer/>
+    </Router>
+    )
+};
 
 export default App;
