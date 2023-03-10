ef78d389c66ccd801e8200e4ab5f97515658d362
Matasz21 <matas.sabaliauskas@gmail.com>
2023-03-10 22:59:53 +0200

    2023-03-10

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 // import './home.css';
-import Owl from "../../contents/Carousel/Owl";
+// import Owl from "../../contents/Carousel/Owl";
 import ProductPreview from "../../contents/productPreview/productPreview";
 import WhyChoose from "../../contents/whyChoose/whyChoose";
 import Reviews from "../../contents/reviews/reviews";
