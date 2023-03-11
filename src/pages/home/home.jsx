72caf62fecbe0d689c39de6875be066197ac4429
Kostas <sabalkostas@gmail.com>
2023-03-11 16:36:22 +0200

    Calendar logic

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,15 +1,13 @@
 import React from "react";
-// import './home.css';
-// import Owl from "../../contents/Carousel/Owl";
 import ProductPreview from "../../contents/productPreview/productPreview";
 import WhyChoose from "../../contents/whyChoose/whyChoose";
 import Reviews from "../../contents/reviews/reviews";
 import Modal from "../../contents/modal/modal";
-
+import BerryCalerndar from "../../contents/BerryCalendar/BerryCalerndar";
 const Home = () => {
   return (
   <div>
-    {/* <Owl className="owl-theme"/> */}
+    <BerryCalerndar />
     <ProductPreview />
     <Modal />
     <WhyChoose />
